8f5a8743a9d8c88c7ca348997b321085e69366e4
Obieda01 <obieda01@email.franklin.edu>
2017-04-09 18:37:16 -0400

    add questionDeatial:

---
+++ app/views/questionDetail/questionDetail.js
@@ -0,0 +1,34 @@
+'use strict';
+var stackoverflow = require('services/stackoverflow_questionDetail/stackoverflow');
+var angular = require('angular');
+var angularUIRouter = require('angular-ui-router');
+var templateUrl = require('./questionDetail.html');
+
+module.exports = angular.module('aver.stack_overflow_test.question', [angularUIRouter, stackoverflow])
+
+.controller('QuestionCtrl', function($scope, StackOverflow, $http, $stateParams) {
+    $scope.id = $stateParams.id;
+    var vm = this;
+    vm.title = 'View 2 Title';
+    StackOverflow.getInfo()
+        .then(function(data) {
+            vm.siteInfo = data;
+        }, function(error) {
+            vm.stackOverflowError = error;
+        });
+})
+
+.config(function($stateProvider) {
+
+    var questionState = {
+        name: 'question',
+        url: '/questions/:id',
+        controller: 'QuestionCtrl',
+        controllerAs: 'QuestionCtrl',
+        templateUrl: templateUrl
+    };
+    $stateProvider.state(questionState);
+
+})
+
+.name;
