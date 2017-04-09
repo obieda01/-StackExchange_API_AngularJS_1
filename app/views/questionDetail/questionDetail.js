b691cc39aba40a7dfc21e7106826112980c7a990
Obieda01 <obieda01@email.franklin.edu>
2017-04-09 18:26:13 -0400

    feature

--- app/views/questionDetail/questionDetail.js
+++ app/views/questionDetail/questionDetail.js
@@ -1,9 +1,22 @@
 'use strict';
-var stackoverflow = require('services/stackoverflow-questions/stackoverflow');
+var stackoverflow = require('services/stackoverflow_questionDetail/stackoverflow');
 var angular = require('angular');
 var angularUIRouter = require('angular-ui-router');
-var templateUrl = require('./question.html');
+var templateUrl = require('./questionDetail.html');
+
 module.exports = angular.module('aver.stack_overflow_test.question', [angularUIRouter, stackoverflow])
+
+.controller('QuestionCtrl', function($scope, StackOverflow, $http, $stateParams) {
+    $scope.id = $stateParams.id;
+    var vm = this;
+    vm.title = 'View 2 Title';
+    StackOverflow.getInfo()
+        .then(function(data) {
+            vm.siteInfo = data;
+        }, function(error) {
+            vm.stackOverflowError = error;
+        });
+})
 
 .config(function($stateProvider) {
 
@@ -16,18 +29,6 @@ module.exports = angular.module('aver.stack_overflow_test.question', [angularUIR
     };
     $stateProvider.state(questionState);
 
-})
-
-.controller('QuestionCtrl', function($scope, StackOverflow, $http, $stateParams) {
-    $scope.id = $stateParams.id;
-    var vm = this;
-    vm.title = 'View 2 Title';
-    StackOverflow.getInfo()
-        .then(function(data) {
-            vm.siteInfo = data;
-        }, function(error) {
-            vm.stackOverflowError = error;
-        });
 })
 
 .name;
